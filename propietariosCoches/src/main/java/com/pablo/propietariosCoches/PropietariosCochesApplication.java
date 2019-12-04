7beb656e484ddf56b412fe750c4bbd3342648b99
2º DAW MAÑANAS <2 DAW MAANAS@PC807>
2019-12-04 11:13:29 +0100

    RECIÉN DESCARGADO DE SPRING
    Proyecto de propietarios y coches en blanco

---
+++ propietariosCoches/src/main/java/com/pablo/propietariosCoches/PropietariosCochesApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.propietariosCoches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropietariosCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropietariosCochesApplication.class, args);
+	}
+
+}
---
+++ propietariosCoches/src/test/java/com/pablo/propietariosCoches/PropietariosCochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.propietariosCoches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropietariosCochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
