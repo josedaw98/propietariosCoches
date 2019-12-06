d43243032110f158f586c80a84961e3ac41d97c5
pabli <pabli@192.168.56.1>
2019-12-06 14:00:24 +0100

    FONDO DE PANTALLA
    mas pruebas css

--- src/main/java/com/pablo/propietariosCoches/rutas/RutasPropietarios.java
+++ src/main/java/com/pablo/propietariosCoches/rutas/RutasPropietarios.java
@@ -1,7 +1,5 @@
 package com.pablo.propietariosCoches.rutas;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
